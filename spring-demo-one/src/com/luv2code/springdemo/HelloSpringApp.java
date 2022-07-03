71324700c9f1234b0610320ef2eea46c828c7b6b
jjw247 <adr10won@gmail.com>
2022-07-03 18:59:06 +0900

    spring5 basic init

---
+++ spring-demo-one/src/com/luv2code/springdemo/HelloSpringApp.java
@@ -0,0 +1,26 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+		
+		// Load the spring configuration file
+		ClassPathXmlApplicationContext context =
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// Retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		// Call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		// Call the new method for fortunes
+		System.out.println(theCoach.getDailyFortune());
+		
+		// Close the context
+		context.close();
+	}
+
+}
